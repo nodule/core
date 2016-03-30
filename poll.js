2b76ccc9d70760575d6d62fd1ef9760cb4c30c84
Rob Halff <rob.halff@gmail.com>
2016-03-30 12:29:26 +0200

    update

--- poll.js
+++ poll.js
@@ -12,7 +12,7 @@ module.exports = {
         title: "Input object",
         type: "any",
         async: true,
-        fn: function __IN__(data, x, source, state, input, output) {
+        fn: function __IN__(data, source, state, input, $, output) {
           var r = function() {
 
           }.call(this);
